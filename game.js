351b2cf036cd5d59778af92cc5d4babd7664b8fa
dbaldazzi <d.baldazzi@hotmail.com>
2019-08-17 01:54:14 -0600

    got slap to decrement health for player 1 and 2

--- game.js
+++ game.js
@@ -1,27 +1,29 @@
-let healthp1 = "";
+let currentHealthPl1 = 100;
+let currentHealthPl2 = 100;
 
 
-let players = {
-  player1: {
-    name: "aa",
-    initialHealth: 100,
-    slap: 1,
-    punch: 5,
-    kick: 10,
-  },
-  player2: {
-    name: "bb",
-    initialHealth: 100,
-    slapdamage: 1,
-    punch: 5,
-    kick: 10,
-  }
+let player1 = {
+  name: "aa",
+  initialHealth: 100,
+  slap: 1,
+  punch: 5,
+  kick: 10,
+}
+let player2 = {
+  name: "bb",
+  initialHealth: 100,
+  slapdamage: 1,
+  punch: 5,
+  kick: 10,
 }
 
+
 //player 1 health function 
-function slap(player1) {
-  let currentHealthp1 = players.player1.initialhealth
-  currentHealthp1--
+function slap() {
+  // let currentHealthPl1 = player1.initialHealth
+  // currentHealthPl1.initialHealthplayer1--
+  currentHealthPl1 == player1.initialHealth--
+  console.log(player1.initialHealth)
 
   draw()
 }
@@ -36,10 +38,11 @@ function kickPl1(player1) {
 }
 
 //player 2 health function 
-function slapPl2(player2) {
-  let currentHealthpl2 = players.player2.initialhealth
-  currentHealthpl2--
-  players.player1.health -= currentHealthpl2
+function slapPl2() {
+  // let currentHealthPl2 = player2.initialHealth
+  // currentHealthPl2--
+  currentHealthPl2 == player2.initialHealth--
+  console.log(player2.initialHealth)
   draw()
 }
 
@@ -57,7 +60,7 @@ console.log(slap)
 function draw() {
   //player 1
   let healthValueElement = document.getElementById('health')
-  healthValueElement.textContent = `: ${players.player1.initialHealth}`
+  healthValueElement.textContent = `: ${player1.initialHealth}`
   let slapElement = document.querySelector("#slap")
   let punchElement = document.querySelector("#punchPl1")
   let kickPl1Element = document.querySelector("#kickPl1")
@@ -65,7 +68,7 @@ function draw() {
   //plaer 2
 
   let healthPlayer2ValueElement = document.getElementById('healthPlayer2')
-  healthPlayer2ValueElement.textContent = `: ${players.player2.initialHealth}`
+  healthPlayer2ValueElement.textContent = `: ${player2.initialHealth}`
   let slapPl2Element = document.querySelector("#slapPl2")
   let punchPl2Element = document.querySelector("#punchPl2")
   let kickPl2Element = document.querySelector("#kickPl2")
