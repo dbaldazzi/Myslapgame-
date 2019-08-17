038a611dc19fa7c8daa0005e5371990a3c4cdd73
dbaldazzi <d.baldazzi@hotmail.com>
2019-08-16 23:50:46 -0600

    linked js to buttons on html

--- game.js
+++ game.js
@@ -34,11 +34,17 @@ console.log(slap)
 
 
 function draw() {
+  //player 1
   let healthValueElement = document.getElementById('health')
   healthValueElement.textContent = `health: ${players.player1.initialHealth}`
   let slapElement = document.querySelector("#slap")
-  let slap2Element = document.querySelector("#slap2")
+  let punchElement = document.querySelector("#punchpl1")
+  let kickpl1Element = document.querySelector("#kickpl1")
+  //plaer 2
+
   let healthPlayer2ValueElement = document.getElementById('healthPlayer2')
   healthPlayer2ValueElement.textContent = `healthPlayer2: ${players.player2.initialHealth}`
-
+  let slappl2Element = document.querySelector("#slappl2")
+  let punchpl2Element = document.querySelector("#punchpl2")
+  let kickpl2Element = document.querySelector("#kickpl2")
 }
