94af67dd57b75d9c4a26bd19174c79aab7dca467
dbaldazzi <d.baldazzi@hotmail.com>
2019-08-17 01:16:30 -0600

    attempted to get buttons working

--- game.js
+++ game.js
@@ -5,12 +5,16 @@ let players = {
   player1: {
     name: "aa",
     initialHealth: 100,
-    slapdamage: 1,
+    slap: 1,
+    punch: 5,
+    kick: 10,
   },
   player2: {
     name: "bb",
     initialHealth: 100,
     slapdamage: 1,
+    punch: 5,
+    kick: 10,
   }
 }
 
@@ -18,15 +22,32 @@ let players = {
 function slap(player1) {
   let currentHealthp1 = players.player1.initialhealth
   currentHealthp1--
-  console.log(currentHealthp1)
+
   draw()
 }
 
 
+function punchPl1(player1) {
+  draw()
+}
+
+function kickPl1(player1) {
+  draw()
+}
+
 //player 2 health function 
-function slap2(player2) {
-  let currentHealthp2 = players.player2.health
-  players.player1.health -= currentHealthp2
+function slapPl2(player2) {
+  let currentHealthpl2 = players.player2.initialhealth
+  currentHealthpl2--
+  players.player1.health -= currentHealthpl2
+  draw()
+}
+
+function punchPl2(player2) {
+  draw()
+}
+
+function kickPl2(player2) {
   draw()
 }
 
@@ -36,15 +57,17 @@ console.log(slap)
 function draw() {
   //player 1
   let healthValueElement = document.getElementById('health')
-  healthValueElement.textContent = `health: ${players.player1.initialHealth}`
+  healthValueElement.textContent = `: ${players.player1.initialHealth}`
   let slapElement = document.querySelector("#slap")
-  let punchElement = document.querySelector("#punchpl1")
-  let kickpl1Element = document.querySelector("#kickpl1")
+  let punchElement = document.querySelector("#punchPl1")
+  let kickPl1Element = document.querySelector("#kickPl1")
+
   //plaer 2
 
   let healthPlayer2ValueElement = document.getElementById('healthPlayer2')
-  healthPlayer2ValueElement.textContent = `healthPlayer2: ${players.player2.initialHealth}`
-  let slappl2Element = document.querySelector("#slappl2")
-  let punchpl2Element = document.querySelector("#punchpl2")
-  let kickpl2Element = document.querySelector("#kickpl2")
+  healthPlayer2ValueElement.textContent = `: ${players.player2.initialHealth}`
+  let slapPl2Element = document.querySelector("#slapPl2")
+  let punchPl2Element = document.querySelector("#punchPl2")
+  let kickPl2Element = document.querySelector("#kickPl2")
 }
+draw() 
